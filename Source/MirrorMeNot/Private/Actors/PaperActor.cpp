cbb4c80064394930c44c6f08678958978f9d452e
ShinuziKyura <rj.santos.pereira@gmail.com>
2019-03-10 06:41:54 +0000

    Improve PaperActor interface and movement testing
    MMN-002

--- Source/MirrorMeNot/Private/Actors/PaperActor.cpp
+++ Source/MirrorMeNot/Private/Actors/PaperActor.cpp
@@ -1,31 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Actors/PaperActor.h"
+#include "Engine/CollisionProfile.h"
 #include "Components/SphereComponent.h"
 #include "PaperFlipbookComponent.h"
+#include "GameFramework/WorldSettings.h"
 
-// Sets default values
+DEFINE_LOG_CATEGORY(LogPaperActor)
+
+const FName APaperActor::PaperActor_ProfileName(TEXT("PaperActor"));
+
 APaperActor::APaperActor(FObjectInitializer const& ObjectInitializer)
 	: Super(ObjectInitializer)
-	, FlipbookComponent(nullptr)
-	, bCanJump(true)
-	, bCanDrop(true)
+	, JumpDuration(0.0f)
+	, bIsJumping(false)
+	, bIsFalling(false)
+	, bIsMoving(false)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	SphereComponent = CreateDefaultSubobject<USphereComponent>(FName(TEXT("SphereComponent0")));
-	FlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(FName(TEXT("PaperFlipbookComponent0")));
-	FlipbookComponent->AttachToComponent(SphereComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent0"));
+	FlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("PaperFlipbookComponent0"));
 
-	RootComponent = SphereComponent;
+	SetRootComponent(SphereComponent);
+
+	SphereComponent->SetSimulatePhysics(true);
+	SphereComponent->SetNotifyRigidBodyCollision(true);
+	SphereComponent->SetCollisionProfileName(PaperActor_ProfileName);
+	
+	FlipbookComponent->SetupAttachment(SphereComponent);
+	FlipbookComponent->SetGenerateOverlapEvents(false);
+	FlipbookComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 }
 
 // Called when the game starts or when spawned
 void APaperActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SphereComponent->OnComponentHit.AddDynamic(this, &APaperActor::OnComponentHitHandler);
 }
 
 // Called every frame
@@ -33,21 +46,86 @@ void APaperActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	DistanceToPoint = SphereComponent->GetClosestPointOnCollision(SphereComponent->GetComponentTransform().GetLocation(), ClosestPoint);
+	// TODO adjust these values according to GetWorld()->GetGravityZ()
+	
+	// Clamp maximum speed
+	auto LinearVelocity = SphereComponent->GetPhysicsLinearVelocity();
+	LinearVelocity.X = FMath::Clamp(LinearVelocity.X, -370.f * 2.f, 370.f * 2.f);
+	SphereComponent->SetPhysicsLinearVelocity(LinearVelocity);
+
+	if (bIsJumping)
+	{
+		SphereComponent->AddForce(FVector(0.f, 0.f, 9800.f), NAME_None, true);
+		JumpDuration += DeltaTime;
+		bIsJumping = JumpDuration <= .1f;
+	}
+
+	switch (bIsMoving)
+	{
+		case EMovementDirection::Left:
+			SphereComponent->AddForce(FVector(-3700.f * 2.f, 0.f, 0.f), NAME_None, true);
+			break;
+		case EMovementDirection::Right:
+			SphereComponent->AddForce(FVector(3700.f * 2.f, 0.f, 0.f), NAME_None, true);
+			break;
+		case EMovementDirection::None:
+			if (FMath::IsNearlyZero(JumpDuration)) // TODO preference regarding this behaviour
+			{
+				SphereComponent->AddImpulse(FVector(-LinearVelocity.X * 10.f, 0.f, 0.f));
+			}
+			break;
+	}
+	
+//	UE_LOG(LogPaperActor, Log, TEXT("%s"), *LinearVelocity.ToString())
 }
 
 void APaperActor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// TODO remove this
+	BindAction(PlayerInputComponent, TEXT("Up"), this, &APaperActor::JumpUp);
+	BindAction(PlayerInputComponent, TEXT("Left"), this, &APaperActor::MoveLeft);
+	BindAction(PlayerInputComponent, TEXT("Right"), this, &APaperActor::MoveRight);
 }
 
-void APaperActor::MoveUp()
+void APaperActor::JumpUp(const bool bPressed)
 {
-	if (bCanJump)
+	bIsJumping = bPressed;
+}
+
+void APaperActor::MoveLeft(const bool bPressed)
+{
+	if (bPressed)
 	{
-		FlipbookComponent->AddImpulse(FVector(0.f, 0.f, 50.f));
-		bCanJump = false;
+		bIsMoving = EMovementDirection::Left;
+	}
+	else if (bIsMoving == EMovementDirection::Left)
+	{
+		bIsMoving = EMovementDirection::None;
 	}
 }
 
+void APaperActor::MoveRight(const bool bPressed)
+{
+	if (bPressed)
+	{
+		bIsMoving = EMovementDirection::Right;
+	}
+	else if (bIsMoving == EMovementDirection::Right)
+	{
+		bIsMoving = EMovementDirection::None;
+	}
+}
 
+void APaperActor::OnComponentHitHandler(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// TODO proper checking ground collision (ray traces sound good)
+	JumpDuration = 0.f;
+
+	auto LinearVelocity = SphereComponent->GetPhysicsLinearVelocity();
+	LinearVelocity.Z = 0.f;
+	SphereComponent->SetPhysicsLinearVelocity(LinearVelocity);
+
+	UE_LOG(LogPaperActor, Log, TEXT("That's a hit!"));
+}
--- Source/MirrorMeNot/Public/Actors/PaperActor.h
+++ Source/MirrorMeNot/Public/Actors/PaperActor.h
@@ -4,14 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/InputComponent.h"
 #include "PaperActor.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogPaperActor, Log, All)
 
 UCLASS()
 class MIRRORMENOT_API APaperActor : public APawn
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	APaperActor(FObjectInitializer const& ObjectInitializer);
 
@@ -22,29 +25,46 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	void SetupPlayerInputComponent(class UInputComponent * PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent * PlayerInputComponent) override;
+
+	template <class UserClass>
+	void BindAction(UInputComponent* PlayerInputComponent, const FName ActionName, UserClass* Object, typename TBaseDelegate<void, bool>::TUObjectMethodDelegate<UserClass>::FMethodPtr Func)
+	{
+		PlayerInputComponent->BindAction<TBaseDelegate<void, bool>>(ActionName, IE_Pressed, Object, Func, true);
+		PlayerInputComponent->BindAction<TBaseDelegate<void, bool>>(ActionName, IE_Released, Object, Func, false);
+	}
 
 protected:
-	void MoveUp();
+	struct EMovementDirection
+	{
+		static const uint8 None = 0b00;
+		static const uint8 Left = 0b01;
+		static const uint8 Right = 0b10;
+	};
 
-	void MoveDown();
+	void JumpUp(const bool bPressed);
 
-	UFUNCTION(BlueprintImplementableEvent)
-	void MoveLeft();
+	void DropDown();
 
-	void MoveRight();
+	void MoveLeft(const bool bPressed);
+
+	void MoveRight(const bool bPressed);
+
+	UFUNCTION()
+	void OnComponentHitHandler(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Paper Actor")
 	class USphereComponent* SphereComponent;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paper Actor")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Paper Actor")
 	class UPaperFlipbookComponent* FlipbookComponent;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Paper Actor|Collision")
-	FVector ClosestPoint;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Paper Actor|Collision")
-	float DistanceToPoint;
+	float JumpDuration;
 
-	uint8 bCanJump : 1;
-	uint8 bCanDrop : 1;
+	uint8 bIsJumping : 1;
+	uint8 bIsFalling : 1;
+	uint8 bIsMoving : 2;
+
+	static const FName PaperActor_ProfileName;
 };
+
