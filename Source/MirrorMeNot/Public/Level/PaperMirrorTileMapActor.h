8da8467ccc5ba58ba53c7891a6c399f460f3088d
Ricardo Santos <rsantos@abyssal.eu>
2019-03-17 23:21:10 +0000

    Create mirrorable tile map class
    MMN-004

---
+++ Source/MirrorMeNot/Public/Level/PaperMirrorTileMapActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperTileMapActor.h"
+#include "PaperMirrorTileMapActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRRORMENOT_API APaperMirrorTileMapActor : public APaperTileMapActor
+{
+	GENERATED_BODY()
+
+public:
+	APaperMirrorTileMapActor(FObjectInitializer const& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+
+	void SwapLayers();
+
+private:
+	TArray<class UPaperTileLayer*> TileLayers;
+	TArray<class UPaperTileLayer*> ReverseTileLayers;
+
+	bool bIsReversed;
+};
